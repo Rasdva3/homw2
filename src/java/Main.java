f05bd10422f7345b3e7e2bfc3df40fad62c654a8
rasdva3 <rasdva3@inbox.ru>
2025-01-10 10:09:19 +0300

    initial commit

---
+++ src/java/Main.java
@@ -0,0 +1,23 @@
+// Task 1
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8;
+        var cat =3.6;
+        var paper = 763789;
+        System.out.println(dog + "\n" + cat + "\n" + paper);
+    }
+}
+
+// Task 2
+class Plural {
+    public static void main(String[] args) {
+        var dog = 8;
+        dog = dog + 4;
+        var cat = 3.6;
+        cat = cat + 4;
+        var paper = 763789;
+        paper = paper + 4;
+        System.out.println(dog + "\n" + cat + "\n" + paper);
+    }
+}
+

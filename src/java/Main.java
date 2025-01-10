21dae7d17f297441b93be9ee1c0af09586078c26
rasdva3 <rasdva3@inbox.ru>
2025-01-10 10:42:14 +0300

    subtraction

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,23 +1,21 @@
 // Task 1
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
+        var dog = 8.0;
         var cat =3.6;
         var paper = 763789;
         System.out.println(dog + "\n" + cat + "\n" + paper);
-    }
-}
 
-// Task 2
-class Plural {
-    public static void main(String[] args) {
-        var dog = 8;
+        //Task 2
         dog = dog + 4;
-        var cat = 3.6;
         cat = cat + 4;
-        var paper = 763789;
         paper = paper + 4;
         System.out.println(dog + "\n" + cat + "\n" + paper);
+
+        //Task 3
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper -7639;
     }
 }
 
